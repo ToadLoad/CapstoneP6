8d5940209455e569658d4831fde0122626ebed18
ToadLoad <billy.pham223@gmail.com>
2023-10-19 00:40:13 -0700

    JS in progress

--- javascript.js
+++ javascript.js
@@ -1,3 +1,9 @@
-document.getElementById
+function ImageChanger() {
+    var image = document.getElementById('Image');
+    if (image.src.match("image1.jpg")) {
+      image.src = "image2.jpg";
+    } else {
+      image.src = "image1.jpg";
+    }
 
-onclick=""
+}
