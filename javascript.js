e1dd7ed37873e54899b774a7e1ddf173ccb04a08
ToadLoad <billy.pham223@gmail.com>
2023-10-20 00:53:36 -0700

    JS in progress

--- javascript.js
+++ javascript.js
@@ -7,3 +7,24 @@ function ImageChanger() {
     }
 
 }
+
+var Title = document.getElementById('myTitle');
+
+function TitleChanger(){
+  if (Title.innerHTML.match("Frog Treats")){
+    Title.innerHTML = "Jelly Candy";
+    Title.style.backgroundColor =  "#33cc33"
+  }
+  else if (Title.innerHTML.match("Jelly Candy")){
+    Title.innerHTML = "Fly Snacks";
+    Title.style.backgroundColor = "#7B7878"
+  }
+
+  else{
+    Title.innerHTML = "Frog Treats";
+    Title.style.background= "transparent"
+  }
+  }
+
+/* later */
+clearInterval(refreshIntervalId);
